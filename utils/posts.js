fbe52908ba386285222f4c4a963cf2abfc17deaf
akwjr963 <akwjr963@daum.net>
2024-09-12 14:16:58 +0900

    과제

---
+++ utils/posts.js
@@ -0,0 +1,7 @@
+export function getAllPosts() {
+  // 여기서는 간단히 하드코딩된 더미 데이터를 반환합니다.
+  return [
+    { id: "post-1", title: "첫 번째 포스트", content: "첫 번째 포스트 내용" },
+    { id: "post-2", title: "두 번째 포스트", content: "두 번째 포스트 내용" },
+  ];
+}
